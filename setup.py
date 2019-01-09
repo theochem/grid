ca4b8c5251bd8f10f1605847b9e501225ce9605a
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-01-09 01:01:41 +0100

    Add HEADER to setup.py script

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# OLDGRIDS: Helpful Open-source Research TOol for N-fermion systems.
+# Copyright (C) 2011-2017 The OLDGRIDS Development Team
+#
+# This file is part of OLDGRIDS.
+#
+# OLDGRIDS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# OLDGRIDS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
 """Package build and install script."""
+
+
 from glob import glob
 
 import Cython.Build
@@ -25,9 +47,6 @@ def get_readme():
     """Load README.rst for display on PyPI."""
     with open('README.rst') as f:
         return f.read()
-
-
-
 
 
 setup(
