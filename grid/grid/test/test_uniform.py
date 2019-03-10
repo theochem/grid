719e6ec3a581f252ff0f362699d116a13f3f8701
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-03-10 21:00:03 +0100

    Translate uniform.cpp to uniform.py
    1. Translate cpp UnifromGrid to python
    2. Remove pbc from UniformGrid
    3. Have grid.grid.test_uniform use python UnifromGrid
    4. Comment get_ranges_rcut tests; the get_ranges_rcut method will be
       implemented if needed at a later stage.

--- grid/grid/test/test_uniform.py
+++ grid/grid/test/test_uniform.py
@@ -22,8 +22,8 @@
 
 import numpy as np
 
+from grid.grid.uniform import UniformGrid
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
-from grid import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 
 def test_uig_integrate_gauss():
@@ -36,8 +36,7 @@ def test_uig_integrate_gauss():
     origin = np.zeros(3, float) - offset
     rvecs = np.identity(3) * spacing
     shape = np.array([naxis, naxis, naxis])
-    pbc = np.ones(3, int)
-    uig = UniformGrid(origin, rvecs, shape, pbc)
+    uig = UniformGrid(origin, rvecs, shape)
 
     # fill a 3D grid with a gaussian function
     x, y, z = np.indices(shape)
@@ -49,35 +48,6 @@ def test_uig_integrate_gauss():
     assert_almost_equal(num_result, 1.0, decimal=3)
 
 
-def test_ugrid_writable():
-    origin = np.random.normal(0, 1, 3)
-    grid_rvecs = np.random.normal(0, 1, (3, 3))
-    shape = np.random.randint(10, 20, 3)
-    pbc = np.random.randint(0, 2, 3)
-    ugrid = UniformGrid(origin, grid_rvecs, shape, pbc)
-    # write to attributes and check value
-    for i in range(10):
-        value = np.random.normal(0, 1)
-        i, j = np.random.randint(0, 3, 2)
-        ugrid.origin[i] = value
-        assert ugrid.origin[i] == value
-        ugrid.grid_rvecs[i, j] = value
-        assert ugrid.grid_rvecs[i, j] == value
-        k = np.random.randint(10, 20)
-        ugrid.shape[i] = k
-        assert ugrid.shape[i] == k
-        l = np.random.randint(0, 2)
-        ugrid.pbc[j] = l
-        assert ugrid.pbc[j] == l
-
-
-def test_index_wrap():
-    assert index_wrap(2, 3) == 2
-    assert index_wrap(-5, 10) == 5
-    assert index_wrap(5, 10) == 5
-    assert index_wrap(15, 10) == 5
-
-
 def get_simple_test_uig():
     origin = np.array([0.1, -2.0, 3.1])
     rvecs = np.array([
@@ -85,37 +55,30 @@ def get_simple_test_uig():
         [0.1, 1.1, 0.2],
         [0.0, -0.1, 1.0],
     ])
-    # origin = np.array([0.0, 0.0, 0.0])
-    # rvecs = np.array([
-    #    [1.0, 0.0, 0.0],
-    #    [0.0, 1.0, 0.0],
-    #    [0.0, 0.0, 1.0],
-    # ])
     shape = np.array([40, 40, 40])
-    pbc = np.array([1, 0, 1])
-    return UniformGrid(origin, rvecs, shape, pbc)
+    return UniformGrid(origin, rvecs, shape)
 
 
-def test_get_ranges_rcut1():
-    uig = get_simple_test_uig()
-    center = np.array([0.1, -2.5, 3.2])
-    rb1, re1 = uig.get_ranges_rcut(center, 2.0)
-    rb2, re2 = uig.get_grid_cell().get_ranges_rcut(uig.origin - center, 2.0)
-    assert rb1[0] == rb2[0]
-    assert rb1[1] == 0
-    assert rb1[2] == rb2[2]
-    assert (re1 == re2).all()
-
-
-def test_get_ranges_rcut2():
-    uig = get_simple_test_uig()
-    center = np.array([60.0, 50.0, 60.0])
-    rb1, re1 = uig.get_ranges_rcut(center, 2.0)
-    rb2, re2 = uig.get_grid_cell().get_ranges_rcut(uig.origin - center, 2.0)
-    assert (rb1 == rb2).all()
-    assert re1[0] == re2[0]
-    assert re1[1] == 40
-    assert re1[2] == re2[2]
+# def test_get_ranges_rcut1():
+#     uig = get_simple_test_uig()
+#     center = np.array([0.1, -2.5, 3.2])
+#     rb1, re1 = uig.get_ranges_rcut(center, 2.0)
+#     rb2, re2 = uig.get_grid_cell().get_ranges_rcut(uig.origin - center, 2.0)
+#     assert rb1[0] == rb2[0]
+#     assert rb1[1] == 0
+#     assert rb1[2] == rb2[2]
+#     assert (re1 == re2).all()
+#
+#
+# def test_get_ranges_rcut2():
+#     uig = get_simple_test_uig()
+#     center = np.array([60.0, 50.0, 60.0])
+#     rb1, re1 = uig.get_ranges_rcut(center, 2.0)
+#     rb2, re2 = uig.get_grid_cell().get_ranges_rcut(uig.origin - center, 2.0)
+#     assert (rb1 == rb2).all()
+#     assert re1[0] == re2[0]
+#     assert re1[1] == 40
+#     assert re1[2] == re2[2]
 
 
 def test_dist_grid_point():
@@ -126,11 +89,6 @@ def test_dist_grid_point():
                         (0.1 ** 2 + 1.0) ** 0.5, decimal=10)
     assert_almost_equal(uig.dist_grid_point(uig.origin, np.array([0, 1, 0])),
                         (0.1 ** 2 + 1.1 ** 2 + 0.2 ** 2) ** 0.5, decimal=10)
-    center = np.array([0.9, 2.5, 1.6])
-    indexes = np.array([6, 3, -2])
-    point = uig.origin + uig.get_grid_cell().to_cart(indexes.astype(float))
-    assert_array_almost_equal(uig.dist_grid_point(center, indexes),
-                              np.linalg.norm(center - point), decimal=10)
 
 
 def test_delta_grid_point():
@@ -141,7 +99,3 @@ def test_delta_grid_point():
                               np.array([0.0, -0.1, 1.0]), decimal=10)
     assert_array_almost_equal(uig.delta_grid_point(uig.origin, np.array([0, 1, 0])),
                               np.array([0.1, 1.1, 0.2]), decimal=10)
-    center = np.array([0.9, 2.5, 1.6])
-    indexes = np.array([6, 3, -2])
-    point = uig.origin + uig.get_grid_cell().to_cart(indexes.astype(float))
-    assert_array_almost_equal(uig.delta_grid_point(center, indexes), point - center, decimal=10)
