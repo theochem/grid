0a9ed378352a6c93d633eb8a3c405ea7e0a33dc6
Derrick <yxt1991@gmail.com>
2019-04-03 04:15:05 -0400

    Add vecterized becke weights version
    1. Use broadcasting to completely vectorize
       becke weights method
    2. Move grid to basegrid incase namespace conflict

--- src/grid/becke.py
+++ src/grid/becke.py
@@ -21,11 +21,49 @@
 # --
 """Becke Weights Module."""
 
-
 import numpy as np
 
 
-__all__ = ['becke_helper_atom']
+class BeckeWeights:
+    def __init__(atom_grids, radii, atom_coors, index):
+        ...
+
+    @staticmethod
+    def _calculate_alpha(radii, cutoff=0.45):
+        u_ab = (radii[:, None] - radii) / (radii[:, None] + radii)
+        alpha = u_ab / (u_ab ** 2 - 1)
+        alpha[alpha > cutoff] = cutoff
+        alpha[alpha < -cutoff] = -cutoff
+        return alpha
+
+    @staticmethod
+    def atomic_dists(coors):
+        return np.linalg.norm(coors[:, None] - coors, axis=-1)
+
+    @staticmethod
+    def switch_func(x, order=3):
+        for i in range(order):
+            x = 1.5 * x - 0.5 * x ** 3
+        return x
+
+    @staticmethod
+    def generate_becke_weights(points, atom_coors, radii, select_index):
+        # |r_A - r| for each points, nucleus pair
+        n_p = np.linalg.norm(atom_coors[:, None] - points, axis=-1)
+        # |r_A - r| - |r_B - r| for each points with pair(A, B) nucleus
+        p_p_n = n_p[:, None] - n_p
+        atomic_dist = BeckeWeights.atomic_dists(atom_coors)
+        mu_n_p_p = p_p_n.transpose([2, 0, 1]) / atomic_dist
+        alpha = BeckeWeights._calculate_alpha(radii)
+        v_pp = mu_n_p_p + alpha * (1 - mu_n_p_p ** 2)
+        s_ab = 0.5 * (1 - BeckeWeights.switch_func(v_pp))
+        # convert nan to 1
+        s_ab[np.isnan(s_ab)] = 1
+        # product up A_B, A_C, A_D ... along second axis
+        s_ab_prod = np.prod(s_ab, axis=-1)
+        # calculate weight for each point for select_index
+        weights = s_ab_prod[:, select_index] / np.sum(s_ab_prod, axis=1)
+        return weights
 
 
 def distance(point0, point1):
@@ -41,100 +79,100 @@ def distance(point0, point1):
     return np.sqrt(np.sum((point0 - point1)**2))
 
 
-def becke_helper_atom(points, weights, radii, centers, select, order):
-    """Compute Becke weights on the grid points for a given atom.
+# def becke_helper_atom(points, weights, radii, centers, select, order=3):
+#     """Compute Becke weights on the grid points for a given atom.
 
-    Arguments
-    ---------
-    points : np.ndarray, shape=(npoints, 3)
-        Cartesian coordinates of the grid points.
-    weights : np.ndarray, shape=(npoints,)
-        The output array where the Becke partitioning weights are
-        written. Becke weights are **multiplied** with the original
-        contents of the array!
-    radii : np.ndarray, shape=(natom,)
-        Covalent radii used to shrink/enlarge basins in the Becke scheme.
-    centers : np.ndarray, shape=(natom, 3)
-        Cartesian coordinates of the nuclei.
-    select : np.ndarray
-        Index of atoms for which the Becke weights are computed.
-    order : np.ndarray
-        Order of switching function in the Becke scheme.
+#     Arguments
+#     ---------
+#     points : np.ndarray, shape=(npoints, 3)
+#         Cartesian coordinates of the grid points.
+#     weights : np.ndarray, shape=(npoints,)
+#         The output array where the Becke partitioning weights are
+#         written. Becke weights are **multiplied** with the original
+#         contents of the array!
+#     radii : np.ndarray, shape=(natom,)
+#         Covalent radii used to shrink/enlarge basins in the Becke scheme.
+#     centers : np.ndarray, shape=(natom, 3)
+#         Cartesian coordinates of the nuclei.
+#     select : np.ndarray
+#         Index of atoms for which the Becke weights are computed.
+#     order : np.ndarray
+#         Order of switching function in the Becke scheme.
 
-    Notes
-    -----
-    See A. D. Becke, The Journal of Chemical Physics 88, 2547 (1988).
-    """
+#     Notes
+#     -----
+#     See A. D. Becke, The Journal of Chemical Physics 88, 2547 (1988).
+#     """
 
-    npoint = points.shape[0]
-    natom = radii.shape[0]
+#     npoint = points.shape[0]
+#     natom = radii.shape[0]
 
-    assert points.shape[1] == 3, "points does not have the right size!"
-    assert weights.shape[0] == npoint, "weights does not have the right size!"
-    assert centers.shape[0] == natom, "centers does not have the right size!"
-    assert centers.shape[1] == 3, "centers does not have the right size!"
-    assert 0 <= select < natom, "select must be in the range [0, natom)"
-    assert order > 0, "order must be greater than zero"
+#     assert points.shape[1] == 3, "points does not have the right size!"
+#     assert weights.shape[0] == npoint, "weights does not have the right size!"
+#     assert centers.shape[0] == natom, "centers does not have the right size!"
+#     assert centers.shape[1] == 3, "centers does not have the right size!"
+#     assert 0 <= select < natom, "select must be in the range [0, natom)"
+#     assert order > 0, "order must be greater than zero"
 
-    # Precompute the alpha parameters for each atom pair
-    alphas = np.zeros(int(natom * (natom + 1) / 2))
-    offset = 0
-    for iatom in range(natom):
-        for jatom in range(iatom + 1):
-            alpha = (radii[iatom] - radii[jatom]) / (radii[iatom] + radii[jatom])
-            alpha = alpha / (alpha**2 - 1)  # Eq. (A5)
-            # Eq. (A3), except that we use some safe margin
-            # (0.45 instead of 0.5) to stay way from a ridiculous imbalance.
-            if alpha > 0.45:
-                alpha = 0.45
-            elif alpha < -0.45:
-                alpha = -0.45
-            alphas[offset] = alpha
-            offset += 1
+#     # Precompute the alpha parameters for each atom pair
+#     alphas = np.zeros(int(natom * (natom + 1) / 2))
+#     offset = 0
+#     for iatom in range(natom):
+#         for jatom in range(iatom + 1):
+#             alpha = (radii[iatom] - radii[jatom]) / (radii[iatom] + radii[jatom])
+#             alpha = alpha / (alpha**2 - 1)  # Eq. (A5)
+#             # Eq. (A3), except that we use some safe margin
+#             # (0.45 instead of 0.5) to stay way from a ridiculous imbalance.
+#             if alpha > 0.45:
+#                 alpha = 0.45
+#             elif alpha < -0.45:
+#                 alpha = -0.45
+#             alphas[offset] = alpha
+#             offset += 1
 
-    # Precompute interatomic distances
-    atomic_dists = np.zeros(int(natom * (natom + 1) / 2))
-    offset = 0
+#     # Precompute interatomic distances
+#     atomic_dists = np.zeros(int(natom * (natom + 1) / 2))
+#     offset = 0
 
-    for iatom in range(natom):
-        for jatom in range(iatom + 1):
-            atomic_dists[offset] = distance(centers[iatom], centers[jatom])
-            offset += 1
+#     for iatom in range(natom):
+#         for jatom in range(iatom + 1):
+#             atomic_dists[offset] = distance(centers[iatom], centers[jatom])
+#             offset += 1
 
-    # Calculate the Becke Weights
-    for ipoint in range(npoint - 1, -1, -1):
-        itmp = npoint - ipoint - 1
-        den = 0
-        for iatom in range(natom):
-            p = 1
-            for jatom in range(natom):
-                if iatom == jatom:
-                    continue
-                # Compute offset for alpha and interatomic distance
-                if iatom < jatom:
-                    offset = int((jatom * (jatom + 1) / 2)) + iatom
-                    term = 1
-                else:
-                    offset = int((iatom * (iatom + 1) / 2)) + jatom
-                    term = 0
+#     # Calculate the Becke Weights
+#     for ipoint in range(npoint - 1, -1, -1):
+#         itmp = npoint - ipoint - 1
+#         den = 0
+#         for iatom in range(natom):
+#             p = 1
+#             for jatom in range(natom):
+#                 if iatom == jatom:
+#                     continue
+#                 # Compute offset for alpha and interatomic distance
+#                 if iatom < jatom:
+#                     offset = int((jatom * (jatom + 1) / 2)) + iatom
+#                     term = 1
+#                 else:
+#                     offset = int((iatom * (iatom + 1) / 2)) + jatom
+#                     term = 0
 
-                # Diatomic switching function
-                s = distance(points[itmp], centers[iatom]) - \
-                    distance(points[itmp], centers[jatom])
-                s = s / atomic_dists[offset]  # Eq. (11)
-                s = s + alphas[offset] * (1 - 2 * term) * (1 - s**2)  # Eq. (A2)
+#                 # Diatomic switching function
+#                 s = distance(points[itmp], centers[iatom]) - \
+#                     distance(points[itmp], centers[jatom])
+#                 s = s / atomic_dists[offset]  # Eq. (11)
+#                 s = s + alphas[offset] * (1 - 2 * term) * (1 - s**2)  # Eq. (A2)
 
-                for k in range(1, order + 1, 1):  # Eq. (19) and (20)
-                    s = 0.5 * s * (3 - s**2)
+#                 for k in range(1, order + 1, 1):  # Eq. (19) and (20)
+#                     s = 0.5 * s * (3 - s**2)
 
-                s = 0.5 * (1 - s)  # Eq. (18)
+#                 s = 0.5 * (1 - s)  # Eq. (18)
 
-                p *= s  # Eq. (13)
+#                 p *= s  # Eq. (13)
 
-            if iatom == select:
-                nom = p
+#             if iatom == select:
+#                 nom = p
 
-            den += p  # Eq. (22)
+#             den += p  # Eq. (22)
 
-        # Weight function at this grid point
-        weights[itmp] *= (nom / den)  # Eq. (22)
+#         # Weight function at this grid point
+#         weights[itmp] *= (nom / den)  # Eq. (22)
