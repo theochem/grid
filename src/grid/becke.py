5111d2258945f156e3629abe3696269ce5a738c1
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-07-06 19:02:11 +0200

    Update HEADER and header of modules & scripts

--- src/grid/becke.py
+++ src/grid/becke.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# GRID is a numerical integration library for quantum chemistry.
+# GRID is a numerical integration module for quantum chemistry.
 #
 # Copyright (C) 2011-2019 The GRID Development Team
 #
@@ -17,7 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
-#
 # --
 """Becke Weights Module."""
 
