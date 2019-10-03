d35d409e3f4da284bf3d26a1befea9e3f1e64adb
Derrick <yxt1991@gmail.com>
2019-10-03 15:55:23 -0400

    Update onedgrid test
    1. Add integral tests for OneDGrid
    2. Add error message for easy debugging

--- src/grid/tests/test_onedgrid.py
+++ src/grid/tests/test_onedgrid.py
@@ -25,7 +25,7 @@ from unittest import TestCase
 from grid.onedgrid import GaussChebyshev, GaussLaguerre, GaussLegendre, HortonLinear
 
 import numpy as np
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_almost_equal
 
 from scipy.special import roots_legendre
 
@@ -70,3 +70,36 @@ class TestOneDGrid(TestCase):
         """Test errors raise."""
         with self.assertRaises(ValueError):
             GaussLaguerre(10, -1)
+
+    @staticmethod
+    def helper_gaussian(x):
+        """Compute gauss function for integral between [-1, 1]."""
+        # integrate (exp(-x^2)) x=[-1, 1], result = 1.49365
+        return np.exp(-x ** 2)
+
+    @staticmethod
+    def helper_quadratic(x):
+        """Compute quadratic function for integral between [-1, 1]."""
+        # integrate (-x^2 + 1) x=[-1, 1], result = 1.33333
+        return -x ** 2 + 1
+
+    def test_oned_integral(self):
+        """A simple integral tests for basic oned grid."""
+        # create candidate function
+        # add more ``quadratures: npoints`` if needed
+        candidates_quadratures = {GaussChebyshev: 15, GaussLegendre: 15}
+        # loop each pair to create pts instance
+        for quadrature, n_points in candidates_quadratures.items():
+            grid = quadrature(n_points)
+            # compute gauss numpymerical integral value
+            f1_value = np.sum(self.helper_gaussian(grid.points) * grid.weights)
+            # ref value
+            ref_value = 1.49365
+            assert_almost_equal(f1_value, ref_value, decimal=3)
+
+            # compute quadratic integral value
+            f2_value = np.sum(self.helper_quadratic(grid.points) * grid.weights)
+            # ref value
+            ref_value = 1.33333
+            assert_almost_equal(f2_value, ref_value, decimal=3)
+            print(f"{quadrature.__name__} passed the tests.")
