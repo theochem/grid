1a0dee8e251f4f3a8aa8a63dc88efa491696e532
Ali-Tehrani <alirezatehrani24@gmail.com>
2022-04-27 12:06:28 -0400

    Add module docs to lebedev.py
    Fixed a TODO

--- src/grid/lebedev.py
+++ src/grid/lebedev.py
@@ -17,9 +17,40 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 # --
-"""Lebedev Angular Grid Module.
+"""
+Lebedev angular grid module for constructing integration grids on the unit sphere.
 
-TODO: Describe how the stored points & weights were calculated.
+The Lebedev grid points here were obtained and calculated from the theochem/HORTON package.
+Their calculations were based on F77 translation by Dr. Christoph van Wuellen
+and these are the comments from that translation.
+
+This subroutine is part of a set of subroutines that generate
+Lebedev grids [1-6] for integration on a sphere. The original
+C-code [1] was kindly provided by Dr. Dmitri N. Laikov and
+translated into fortran by Dr. Christoph van Wuellen.
+This subroutine was translated from C to fortran77 by hand.
+Users of this code are asked to include reference [1] in their
+publications, and in the user- and programmers-manuals
+describing their codes.
+This code was distributed through CCL (http://www.ccl.net/).
+
+References
+----------
+.. [1] V.I. Lebedev, and D.N. Laikov
+   "A quadrature formula for the sphere of the 131st algebraic order of accuracy"
+   Doklady Mathematics, Vol. 59, No. 3, 1999, pp. 477-481.
+.. [2] V.I. Lebedev "A quadrature formula for the sphere of 59th algebraic order of accuracy"
+   Russian Acad. Sci. Dokl. Math., Vol. 50, 1995, pp. 283-286.
+.. [3] V.I. Lebedev, and A.L. Skorokhodov "Quadrature formulas of orders 41, 47, and 53 for
+   the sphere" Russian Acad. Sci. Dokl. Math., Vol. 45, 1992, pp. 587-592.
+.. [4] V.I. Lebedev "Spherical quadrature formulas exact to orders 25-29"
+   Siberian Mathematical Journal, Vol. 18, 1977, pp. 99-107.
+.. [5] V.I. Lebedev "Quadratures on a sphere" Computational Mathematics and Mathematical
+   Physics, Vol. 16, 1976, pp. 10-24.
+.. [6] V.I. Lebedev "Values of the nodes and weights of ninth to seventeenth order Gauss-Markov
+   quadrature formulae invariant under the octahedron group with inversion" Computational
+   Mathematics and Mathematical Physics, Vol. 15, 1975, pp. 44-51.
+
 """
 
 import warnings
