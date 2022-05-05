1005aa88c7b743da56cc922b33a454deec5ba7cf
Ali-Tehrani <alirezatehrani24@gmail.com>
2022-05-05 09:19:10 -0400

    Improve docs in ode

--- src/grid/ode.py
+++ src/grid/ode.py
@@ -32,27 +32,67 @@ from sympy import bell
 
 
 class ODE:
-    """General ordinary differential equation solver."""
+    r"""
+    Linear ordinary differential equation solver with boundary conditions.
+
+    Solves a linear ordinary differential equation of order :math:`K` of the form
+
+    .. math::
+        \sum_{k=0}^{K} a_k(x) \frac{d^k y(x)}{dx^k} = f(x)
+
+    with boundary conditions on the two end-points for some unknown function
+    :math:`y(x)` with independent variable :math:`x`. Currently only supports :math:`K`-th
+    order less than or equal to three.
+
+    It also supports the ability to transform the independent variable :math:`x`
+    to another domain :math:`g(x)` for some :math:`K`-th differentiable transformation
+    :math:`g(x)`.  This is particularly useful to convert infinite domains, e.g.
+    :math:`[0, \infty)`, to a finite interval.
+
+    """
 
     @staticmethod
-    def solve_ode(x, fx, coeffs, bd_cond, transform=None):
-        """Solve generic boundary condition ODE.
-
-        .. math::
-            ...
+    def solve_ode(
+        x: np.ndarray,
+        fx: callable,
+        coeffs: Union[list, np.ndarray],
+        bd_cond: list,
+        transform: BaseTransform = None,
+        tol: float = 1e-4,
+        max_nodes: int = 5000,
+        initial_guess_y: np.ndarray = None,
+    ):
+        r"""Solve a linear ODE with boundary conditions.
 
         Parameters
         ----------
         x : np.ndarray(N,)
-            Points from domain for solver ODE
+            Points of the independent variable/domain.
         fx : callable
-            Non homogeneous term in ODE
-        coeffs : list[int or callable] or np.ndarray(K,)
-            Coefficients of each differential term
-        bd_cond : iterable
-            Boundary condition for specific solution
+            Right-hand function :math:`f(x)`.
+        coeffs : list[callable or float] or ndarray(K + 1,)
+            Coefficients :math:`a_k` of each term :math:`\frac{d^k y(x)}{d x^k}`
+            ordered from 0 to K. Either a list of callable functions :math:`a_k(x)` that depends
+            on :math:`x` or array of constants :math:`\{a_k\}_{k=0}^{K}`.
+        bd_cond : list[list[int, int, float]]
+            Boundary condition specified by list of three entries [i, j, C], where
+            :math:`i \in \{0, 1\}` determines whether the lower or upper bound is being
+            constrained, :math:`j \in \{0, \cdots, K\}` determines which derivative
+            :math:`\frac{d^j y}{dx^j}` is being constrained, and :math:`C`
+            determines the boundary constraint value, i.e. :math:`\frac{d^j y}{dx^j} = C`.
+            If `transform` is given, then the constraint of the derivatives is assumed
+            to be with respect to the new coordinate i.e. :math:`\frac{d^j y}{dr^j} = C`.
         transform : BaseTransform, optional
-            Transformation instance r -> x
+            Transformation from one domain :math:`x` to another :math:`r := g(x)`.
+        tol : float, optional
+            Tolerance of the ODE solver and for the boundary condition residuals.
+            See `scipy.integrate.solve_bvp` for more info.
+        max_nodes : int, optional
+            The maximum number of mesh nodes that determine termination.
+            See `scipy.integrate.solve_bvp` function for more info.
+        initial_guess_y : ndarray, optional
+            Initial guess for :math:`y(x)` at the points `x`. If not provided, then
+            random set of points from 0 to 1 is used.
 
         Returns
         -------
@@ -97,44 +137,42 @@ class ODE:
         return res.sol
 
     @staticmethod
-    def _transformed_coeff_ode(coeff_a, tf, x):
-        """Compute coeff for transformed domain.
+    def _transform_ode_from_derivs(
+        coeffs: Union[list, np.ndarray], deriv_transformation: list, r: np.ndarray
+    ):
+        r"""
+        Transform the coefficients of ODE from one variable to another evaluated on mesh points.
+
+        Given a :math:`K`-th differentiable transformation function :math:`g(r)`
+        and a linear ODE of :math:`K`-th order on independent variable :math:`r`
+
+        .. math::
+            \sum_{k=1}^{K} a_k(r) \frac{d^k y}{d r^k}.
+
+        This transforms it into a new coordinate system :math:`g(r) =: x`
+
+        .. math::
+            \sum_{j=1}^K b_j(x) \frac{d^j y}{d x^j},
+
+        where :math:`b_j(x) = \sum_{k=1}^K a_k(x) B_{k, j}(g(r), g^\prime(r), \cdots,
+        g^{k - j + 1}(r))`.
 
         Parameters
         ----------
-        coeff_a : list[number or callable] or np.ndarray
-            Coefficients for normal ODE
-        tf : BaseTransform
-            Transform instance form r -> x
-        x : np.ndarray(N,)
-            Points in the transformed domain
+        coeffs : list[callable or number] or ndarray
+            Coefficients :math:`a_k` of each term :math:`\frac{d^k y(x)}{d r^k}`
+            ordered from 0 to K. Either a list of callable functions :math:`a_k(r)` that depends
+            on :math:`r` or array of constants :math:`\{a_k\}_{k=0}^{K}`.
+        deriv_transformation : list[callable]
+            List of functions for compute transformation derivatives from 1 to :math:`K`.
+        r : ndarray
+            Points from the original domain that is to be transformed.
 
         Returns
         -------
-        np.ndarray
-            Coefficients for transformed ODE
-        """
-        deriv_func = [tf.deriv, tf.deriv2, tf.deriv3]
-        r = tf.inverse(x)
-        return ODE._transformed_coeff_ode_with_r(coeff_a, deriv_func, r)
+        ndarray((K+1, N))
+            Coefficients :math:`b_j(x)` of the new ODE with respect to variable :math:`x`.
 
-    @staticmethod
-    def _transformed_coeff_ode_with_r(coeff_a, deriv_func_list, r):
-        """Convert higher order ODE into 1st order ODE with original domain r.
-
-        Parameters
-        ----------
-        coeff_a : list[callable or numnber] or np.ndarray
-            Coefficients for each differential part
-        deriv_func_list : list[Callable]
-            A list of functions for compute transformation derivatives
-        r : np.ndarray
-            Points from the non-transformed domain
-
-        Returns
-        -------
-        np.ndarray
-            Coefficients for transformed ODE
         """
         derivs = np.array([dev(r) for dev in deriv_func_list])
         total = len(coeff_a)
@@ -171,26 +209,76 @@ class ODE:
         return coeff_b
 
     @staticmethod
-    def _rearrange_trans_ode(x, y, coeff_a, tf, fx_func):
-        """Rearrange coefficients in transformed domain.
+    def _transform_ode_from_rtransform(
+        coeff_a: Union[list, np.ndarray], tf: BaseTransform, x: np.ndarray
+    ):
+        r"""
+        Transform the coefficients of ODE from one variable to another based on Transform object.
+
+        Given a :math:`K`-th differentiable transformation function :math:`g(r)`
+        and a linear ODE of :math:`K`-th order
+
+        .. math::
+            \sum_{k=1}^{K} a_k(r) \frac{d^k y}{d r^k}.
+
+        This transforms it into a new coordinate system with variable :math:`g(r) =: x` via
+
+        .. math::
+            \sum_{j=1}^K b_j(x) \frac{d^j y}{d x^j},
+
+        where :math:`b_j(x) = \sum_{k=1}^K a_k(x) B_{k, j}(g(r), g^\prime(r), \cdots,
+        g^{k - j + 1}(r))`.
 
         Parameters
         ----------
-        x : np.ndarray(n,)
-            points from desired domain
-        y : np.ndarray(order, n)
-            initial guess for the function values and its derivatives
-        coeff_a : list[number or callable] or np.ndarray(Order + 1)
-            Coefficients for each differential from non-transformed part on ODE
+        coeff_a : list[number or callable] or np.ndarray
+            Coefficients for normal ODE
+        tf : BaseTransform
+            Transform instance of :math:`g(x)`
+        x : np.ndarray(N,)
+            Points of the independent variable/domain.
+
+        Returns
+        -------
+        ndarray((K+1, N))
+            Coefficients :math:`b_j(x)` of the new ODE with respect to variable :math:`x`.
+
+        """
+        deriv_func = [tf.deriv, tf.deriv2, tf.deriv3]
+        r = tf.inverse(x)
+        return ODE._transform_ode_from_derivs(coeff_a, deriv_func, r)
+
+    @staticmethod
+    def _transform_and_rearrange_to_explicit_ode(
+        x: np.ndarray,
+        y: np.ndarray,
+        coeff_a: Union[list, np.ndarray],
+        tf: BaseTransform,
+        fx_func: callable,
+    ):
+        r"""Rearrange coefficients in transformed domain.
+
+        Parameters
+        ----------
+        x : np.ndarray(N,)
+            Points of the independent variable/domain.
+        y : np.ndarray(K + 1, N)
+            Initial guess for the function values and its derivatives
+        coeff_a : list[number or callable] or np.ndarray(K + 1)
+            Coefficients :math:`a_k` of each term :math:`\frac{d^k y(x)}{d x^k}`
+            ordered from 0 to K.  Each coefficient can either be a callable function
+            :math:`a_k(x)` or a constant number :math:`a_k`.
         tf: BaseTransform
-            transform instance r -> x
+            Transform instance of :math:`g(x)`.
         fx_func : Callable
-            Non-homogeneous term at given x
+            Right-hand function of the ODE.
 
         Returns
         -------
         np.ndarray(N,)
-            proper expr for the right side of the transformed ODE equation
+            Expression for the right side of the ODE equation after converting it
+            to explicit form evaluated on all N points.
+
         """
         coeff_b = ODE._transformed_coeff_ode(coeff_a, tf, x)
         result = ODE._rearrange_ode(x, y, coeff_b, fx_func(tf.inverse(x)))
@@ -221,27 +309,39 @@ class ODE:
         return coeff_mtr
 
     @staticmethod
-    def _rearrange_ode(x, y, coeff_b, fx):
-        """Rearrange coefficients for scipy solver.
+    def _rearrange_to_explicit_ode(y: np.ndarray, coeff_b: np.ndarray, fx: np.ndarray):
+        r"""Rearrange ODE into explicit form for conversion to first-order ODE (SciPy solver).
+
+        Returns array with :math:`n`-th entry
+
+        .. math::
+            \frac{f(x_n) -\sum_{k=0}^{K - 1} a_k(x_n) \frac{d^k y(x_n)}{d x^k}}{a_{K}(x_n)},
+
+        of the :math:`n`-th point :math:`x` such that :math:`a_K \neq 0`.
+        This arises from re-arranging the ODE of :math:`K`-th order into explicit form.
 
         Parameters
         ----------
-        x : np.ndarray(N,)
-            Points from desired domain
-        y : np.ndarray(order, N)
-            Initial guess for the function values and its derivatives
-        coeff_b : list[number] or np.ndarray(Order + 1)
-            Coefficients for each differential part on ODE
+        y : np.ndarray(K + 1, N)
+            Initial guess for the function values and its :math:`k`-th order derivatives
+            from 1 to :math:`K`, evaluated on the :math:`n`-th point :math:`x_n`.
+        coeff_b : ndarray(K + 1, N)
+            Coefficients :math:`a_k(x_n)` of each term :math:`\frac{d^k y(x)}{d x^k}`
+            ordered from 0 to K evaluated on the :math:`n`-th point :math:`x_n`.
         fx : np.ndarray(N,)
-            Non-homogeneous term at given x
+            Right hand-side of the ODE equation, function :math:`f(x_n)` evaluated on the
+            :math:`n`-th point :math:`x_n`.
 
         Returns
         -------
         np.ndarray(N,)
-            proper expr for the right side of the ODE equation
+            Expression for the right side of the ODE equation after converting it
+            to explicit form evaluated on N-th points.
+
         """
         result = fx
+        # Go through all rows except the last-element.
         for i, b in enumerate(coeff_b[:-1]):
-            # if isinstance(b, Number):
+            # array of size N: a_k(x_n) * (d^k y(x_n) / d x^k)
             result -= b * y[i]
         return result / coeff_b[-1]
