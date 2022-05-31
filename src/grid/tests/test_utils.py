Fix angle is zero for derivative of sph harmonics

src/grid/tests/test_utils.py
@@ -217,8 +217,10 @@ class TestUtils(TestCase):
     ]
 )
 def test_derivative_of_spherical_harmonics_with_finite_difference(numb_pts, max_degree):
-    theta = np.random.uniform(0.0, 2.0 * np.pi, size=(numb_pts,))
-    phi = np.random.uniform(0.0, np.pi, size=(numb_pts,))
+    theta = np.array([1e-5])
+    theta = np.hstack((theta, np.random.uniform(0.0, 2.0 * np.pi, size=(numb_pts,))))
+    phi = np.array([1e-5])
+    phi = np.hstack((phi, np.random.uniform(0.0, np.pi, size=(numb_pts,))))
     eps = 1e-7
     l_max = max_degree
     value = generate_real_spherical_harmonics(l_max, theta, phi)

src/grid/utils.py
@@ -316,7 +316,10 @@ def generate_real_spherical_harmonics(l_max, theta, phi):
 
 def generate_derivative_real_spherical_harmonics(l_max, theta, phi):
     r"""
-    Generate derivatives of real spherical harmonics
+    Generate derivatives of real spherical harmonics.
+
+    If :math:`\phi` is zero, then the first component of the derivative wrt to
+    :math:`phi` is set to zero.
 
     Parameters
     ----------
@@ -355,7 +358,10 @@ def generate_derivative_real_spherical_harmonics(l_max, theta, phi):
 
             # Take derivative wrt to phi:
             # for complex spherical  harmonic it is
-            cot_tangent = 1.0 / np.tan(phi)
+            with np.errstate(divide='ignore', invalid='ignore'):
+                cot_tangent = 1.0 / np.tan(phi)
+            cot_tangent[np.abs(np.tan(phi)) < 1e-10] = 0.0
+
             fac = np.sqrt((l_val - m) * (l_val + m + 1))
 
             if m == 0: