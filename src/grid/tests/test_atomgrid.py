f45d998ab20b5ec877a7b3feffbfcdd1ae00f72b
Ali-Tehrani <alirezatehrani24@gmail.com>
2022-05-31 17:15:04 -0400

    Add two tests for interpolation Laplacian
    - test Laplacian of spherical harmonic is eigenvector
    - test Laplacian of exponential

--- src/grid/tests/test_atomgrid.py
+++ src/grid/tests/test_atomgrid.py
@@ -643,6 +643,59 @@ class TestAtomGrid(TestCase):
                 # Raise error since it can't do derivative beyond one.
                 interp_func(xyz, deriv=3)
 
+    def test_interpolation_of_laplacian_with_spherical_harmonic(self):
+        r"""Test the interpolation of Laplacian of spherical harmonic is eigenvector."""
+        odg = OneDGrid(np.linspace(0.0, 10, num=1000), np.ones(1000), (0, np.inf))
+        rad = IdentityRTransform().transform_1d_grid(odg)
+        degree = 6 * 2 + 2
+        atgrid = AtomGrid.from_pruned(rad, 1, sectors_r=[], sectors_degree=[degree])
+
+        def func(sph_points):
+            # Spherical harmonic of order 6 and magnetic 0
+            r, phi, theta = sph_points.T
+            return np.sqrt(2.0) * np.sqrt(13) / (np.sqrt(np.pi) * 32) * (
+                    231 * np.cos(theta) ** 6.0 - 315 * np.cos(theta) ** 4.0 + 105 * np.cos(
+                theta) ** 2.0 - 5.0
+            )
+        # Get spherical points from atomic grid
+        spherical_pts = atgrid.convert_cartesian_to_spherical(atgrid.points)
+        func_values = func(spherical_pts)
+
+        laplacian = atgrid.interpolate_laplacian(func_values)
+
+        # Test on the same points used for interpolation and random points.
+        for grid in [atgrid.points, np.random.uniform(-1.0, 1.0, (250, 3))]:
+            actual = laplacian(grid)
+            spherical_pts = atgrid.convert_cartesian_to_spherical(grid)
+            # Eigenvector spherical harmonic times l(l + 1) / r^2
+            with np.errstate(divide='ignore', invalid='ignore'):
+                desired = -func(spherical_pts) * 6 * (6 + 1) / spherical_pts[:, 0]**2.0
+            desired[spherical_pts[:, 0]**2.0 < 1e-10] = 0.0
+            assert_almost_equal(actual, desired, decimal=3)
+
+    def test_interpolation_of_laplacian_of_exponential(self):
+        r"""Test the interpolation of Laplacian of exponential."""
+        odg = OneDGrid(np.linspace(0.01, 1, num=1000), np.ones(1000), (0, np.inf))
+        degree = 10
+        atgrid = AtomGrid.from_pruned(odg, 1, sectors_r=[], sectors_degree=[degree])
+
+        def func(cart_pts):
+            radius = np.linalg.norm(cart_pts, axis=1)
+            return np.exp(-radius)
+
+        func_values = func(atgrid.points)
+
+        laplacian = atgrid.interpolate_laplacian(func_values)
+
+        # Test on the same points used for interpolation and random points.
+        for grid in [atgrid.points, np.random.uniform(-0.5, 0.5, (250, 3))]:
+            actual = laplacian(grid)
+            spherical_pts = atgrid.convert_cartesian_to_spherical(grid)
+            # Laplacian of exponential is e^-x (x - 2) / x
+            desired = np.exp(-spherical_pts[:, 0]) * (spherical_pts[:, 0] - 2.0) /\
+                      spherical_pts[:, 0]
+            assert_almost_equal(actual, desired, decimal=3)
+
     def test_error_raises(self):
         """Tests for error raises."""
         with self.assertRaises(TypeError):
