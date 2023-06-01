7742275c709d76f21118ecb1eb64ff8358d6c36a
Ali-Tehrani <alirezatehrani24@gmail.com>
2023-06-01 12:34:36 -0400

    Update for debeging

--- src/grid/tests/test_atomgrid.py
+++ src/grid/tests/test_atomgrid.py
@@ -857,8 +857,8 @@ class TestAtomGrid:
         print(orders)
 
         radial = atgrid.convert_cartesian_to_spherical(atgrid.points, center=center[0])[:, 0]
-        from grid.utils import convert_cart_to_sph
-        radial = convert_cart_to_sph(atgrid.points, center=center[0])[:, 0]
+        print(radial)
+        print(np.mean(radial), np.std(radial), np.max(radial), np.min(radial))
         # Go through each (n, deg, m)
         for i, (n, deg, ord) in enumerate(orders):
             index = deg**2 + 2 * ord - 1 if ord > 0 else deg**2 - 2 * ord
@@ -866,7 +866,8 @@ class TestAtomGrid:
             desired, orders = atgrid.moments(deg, center, radial**n, "pure", return_orders=True)
             print("Pure Orders")
             print(orders)
-            print(f"Index: {index}, i: {i}, n: {n}")
+            print(f"i_pure: {index}, i_pure_radial: {i}, n: {n}")
+            print(desired[index], true[i])
             assert_allclose(desired[index], true[i])
 
     def test_radial_moments_of_gaussian_against_horton(self):
