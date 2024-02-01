859cb0390f3018fc1ebe262d13a24bde5cd2f4d2
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2024-02-01 07:25:51 -0500

    Remove Examples from poisson.py

--- src/grid/poisson.py
+++ src/grid/poisson.py
@@ -209,26 +209,6 @@ def solve_poisson_ivp(
     callable(ndarray(N, 3) -> float) :
         The solution to Poisson equaiton/potential :math:`g : \mathbb{R}^3 \rightarrow \mathbb{R}`.
 
-    Examples
-    --------
-    >>> # Set up of the radial grid
-    >>> oned_grid = Trapezoidal(10000)
-    >>> tf = LinearFiniteRTransform(0.0, 1000)
-    >>> radial_grid = tf.transform_1d_grid(oned)
-    >>> # Set up the atomic grid with degree 10 at each radial point. Molecular grid works as well.
-    >>> atomic_grid = AtomGrid(radial_grid, degrees=[10])
-    >>> # Set the charge distribution to be unit-charge density and evaluate on atomic grid points.
-    >>> def charge_distribution(x, alpha=0.1):
-    >>>    r = np.linalg.norm(x, axis=1)
-    >>>    return (alpha / np.pi)**(3.0 / 2.0) * np.exp(-alpha * r**2.0)
-    >>> func_vals = charge_distribution(atomic_grid.points)
-    >>> # Solve for the potential as an initial value problem and evaluate it over the atomic grid.
-    >>> potential = solve_poisson_ivp(
-    >>>      atgrid, func_vals, InverseRTransform(tf), r_interval=(1000, 1e-3),
-    >>>      ode_params={"method" : "DOP853", "atol": 1e-8},
-    >>> )
-    >>> potential_values = potential(atgrid.points)
-
     """
     return _interpolate_molgrid_helper(
         molgrid,
