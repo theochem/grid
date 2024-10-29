7e06e8cd3d5c9de16d4ddbcff41d8fb8e5a3e7b6
Marco Martínez González <mmg870630@gmail.com>
2024-10-29 14:28:54 -0400

    Add vectorial logo images

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../"))
 project = "grid"
 copyright = "2024, QC-Devs"
 author = "QC-Devs"
-html_logo = "./images/grid_logo_website.png"
+html_logo = "./images/grid_logo_website.svg"
 
 # -- General configuration ---------------------------------------------------
 
