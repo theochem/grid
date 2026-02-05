Fix wrong BOHR_TO_ANGSTROM unit conversion

src/grid/utils.py
@@ -24,7 +24,7 @@ from scipy.special import sph_harm
 from scipy.constants import physical_constants, angstrom
 
 _BOHR_RADIUS_M = physical_constants["Bohr radius"][0]
-BOHR_TO_ANGSTROM = angstrom / _BOHR_RADIUS_M
+BOHR_TO_ANGSTROM = _BOHR_RADIUS_M / angstrom
 ANGSTROM_TO_BOHR = 1.0 / BOHR_TO_ANGSTROM
 
 _bragg = np.array(