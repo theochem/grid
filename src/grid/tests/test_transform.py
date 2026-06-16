e074a3aac95c9a3f70cb778fdb22a35895a24548
Marco Martínez González <mmg870630@gmail.com>
2026-06-16 11:28:43 -0400

    Refactor HandyModRTransform inverse derivative tests to use pytest parametrization

--- src/grid/tests/test_transform.py
+++ src/grid/tests/test_transform.py
@@ -730,6 +730,25 @@ def test_handymod_rtransform_derivatives():
         assert_allclose(third_derivative, third_derivative_fd, rtol=1e-3, atol=2e-3)
 
 
+@pytest.mark.parametrize("x_points", x_points_cases)
+def test_handymod_r_transform_inverse_derivatives(x_points):
+    """Test HandyModRTransform  inverse derivatives against finite difference."""
+    transform = HandyModRTransform(0.1, 10.0, 2)
+    r_points = transform.transform(x_points)
+
+    first_derivative = transform.deriv_inverse(r_points)
+    first_derivative_fd = compute_fd_deriv(transform.inverse, r_points, eps=1e-4, order=1)
+    assert_allclose(first_derivative, first_derivative_fd, rtol=1e-5, atol=1e-8)
+
+    second_derivative = transform.deriv2_inverse(r_points)
+    second_derivative_fd = compute_fd_deriv(transform.inverse, r_points, eps=1e-4, order=2)
+    assert_allclose(second_derivative, second_derivative_fd, rtol=1e-4, atol=1e-6)
+
+    third_derivative = transform.deriv3_inverse(r_points)
+    third_derivative_fd = compute_fd_deriv(transform.inverse, r_points, eps=1e-4, order=3)
+    assert_allclose(third_derivative, third_derivative_fd, rtol=5e-3, atol=1e-4)
+
+
 class TestTransform(TestCase):
     """Transform testcase class."""
 
